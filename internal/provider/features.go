036920d105b054ab6a43355daaf33ea58b7e92c5
Edgar López <edgars.ealo@gmail.com>
2022-03-04 17:37:39 -0700

    feat: added linux virtual machine scale set and its tests and docs

--- internal/provider/features.go
+++ internal/provider/features.go
@@ -10,7 +10,6 @@ func schemaFeatures(supportLegacyTestSuite bool) *pluginsdk.Schema {
 	// NOTE: if there's only one nested field these want to be Required (since there's no point
 	//       specifying the block otherwise) - however for 2+ they should be optional
 	featuresMap := map[string]*pluginsdk.Schema{
-		//lintignore:XS003
 		"virtual_machine": {
 			Type:     pluginsdk.TypeList,
 			Optional: true,
@@ -32,7 +31,27 @@ func schemaFeatures(supportLegacyTestSuite bool) *pluginsdk.Schema {
 				},
 			},
 		},
-
+		"virtual_machine_scale_set": {
+			Type:     pluginsdk.TypeList,
+			Optional: true,
+			MaxItems: 1,
+			Elem: &pluginsdk.Resource{
+				Schema: map[string]*pluginsdk.Schema{
+					"force_delete": {
+						Type:     pluginsdk.TypeBool,
+						Optional: true,
+					},
+					"roll_instances_when_required": {
+						Type:     pluginsdk.TypeBool,
+						Required: true,
+					},
+					"scale_to_zero_before_deletion": {
+						Type:     pluginsdk.TypeBool,
+						Optional: true,
+					},
+				},
+			},
+		},
 		"resource_group": {
 			Type:     pluginsdk.TypeList,
 			Optional: true,
@@ -93,6 +112,22 @@ func expandFeatures(input []interface{}) features.UserFeatures {
 			}
 			if v, ok := virtualMachinesRaw["skip_shutdown_and_force_delete"]; ok {
 				featuresMap.VirtualMachine.SkipShutdownAndForceDelete = v.(bool)
+			}
+		}
+	}
+
+	if raw, ok := val["virtual_machine_scale_set"]; ok {
+		items := raw.([]interface{})
+		if len(items) > 0 {
+			scaleSetRaw := items[0].(map[string]interface{})
+			if v, ok := scaleSetRaw["roll_instances_when_required"]; ok {
+				featuresMap.VirtualMachineScaleSet.RollInstancesWhenRequired = v.(bool)
+			}
+			if v, ok := scaleSetRaw["force_delete"]; ok {
+				featuresMap.VirtualMachineScaleSet.ForceDelete = v.(bool)
+			}
+			if v, ok := scaleSetRaw["scale_to_zero_before_deletion"]; ok {
+				featuresMap.VirtualMachineScaleSet.ScaleToZeroOnDelete = v.(bool)
 			}
 		}
 	}
--- internal/services/compute/registration.go
+++ internal/services/compute/registration.go
@@ -38,6 +38,7 @@ func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
 		"azurestack_virtual_machine_extension":            virtualMachineExtension(),
 		"azurestack_virtual_machine_scale_set":            virtualMachineScaleSet(),
 		"azurestack_virtual_machine_scale_set_extension":  virtualMachineScaleSetExtension(),
+		"azurestack_linux_virtual_machine_scale_set":      resourceLinuxVirtualMachineScaleSet(),
 		"azurestack_windows_virtual_machine":              windowsVirtualMachine(),
 	}
 
